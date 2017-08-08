24b5571961aa4abb987524bc76615620fe25110d
yaodaojia <18910766544@163.com>
2017-08-08 16:01:34 +0800

    Initial commit

---
+++ app/src/main/java/com/edicine_at_home/medicine/view/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.edicine_at_home.medicine.view.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edicine_at_home.medicine.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
